0be9347b8a73dad6454635500deae5c5edbabf33
maxim79135 <maxim79135@gmail.com>
2020-03-25 17:28:42 +0300

    new

--- SD/Labs/Lab6/main.py
+++ SD/Labs/Lab6/main.py
@@ -1,5 +1,39 @@
-name = input()
-name2 = input()
-l = list(ord(name[i]) - ord(name2[i]) for i in range(len(name)))
-for k in l:
-    print(k, end = ' ')
+import prime_numbers as pn
+
+def read_int(min_value, max_value):
+    try:
+        value = int(input())
+        if min_value <= value <= max_value:
+            return value
+        return None
+    except ValueError:
+        return None
+
+def menu():
+    print("Possible options:")
+    print("1. Get random prime number(from 2 to n)")
+    print("2. Get random prime number(from m to n)")
+    print("3. Get array of prime numbers")
+    print("4. Is relative primes(numbers)")
+    print("5. Is pairwise primes(numbers)")
+    print("6. Get next prime following the number")
+    print("7. Get previous prime following the number")
+    print("8. Is prime(number)")
+    print("9. Quit")
+    print("Select item:", end=' ')
+
+if __name__ == "__main__":
+    while True:
+        menu()
+        op_code = read_int(1, 8)
+        if op_code is None:
+            print("Unknow command!\n\n")
+            continue
+        elif op_code == 8:
+            value = read_int(1, 10**9)
+            if value is None:
+                print("Error")
+            else:
+                print(pn.sieveOfAtkin(value))
+        elif op_code == 9:
+            break
---
+++ SD/Labs/Lab6/prime_numbers.py
@@ -0,0 +1,19 @@
+def sieveOfAtkin(limit):
+    import math
+    P = [2,3]
+    sieve=[False]*(limit+1)
+    for x in range(1,int(math.sqrt(limit))+1):
+        for y in range(1,int(math.sqrt(limit))+1):
+            n = 4*x**2 + y**2
+            if n<=limit and (n%12==1 or n%12==5) : sieve[n] = not sieve[n]
+            n = 3*x**2+y**2
+            if n<= limit and n%12==7 : sieve[n] = not sieve[n]
+            n = 3*x**2 - y**2
+            if x>y and n<=limit and n%12==11 : sieve[n] = not sieve[n]
+    for x in range(5,int(math.sqrt(limit))):
+        if sieve[x]:
+            for y in range(x**2,limit+1,x**2):
+                sieve[y] = False
+    for p in range(5,limit):
+        if sieve[p] : P.append(p)
+    return P
