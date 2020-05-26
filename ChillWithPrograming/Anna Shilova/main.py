ed5f0f7a1efdffd88b83a67c3da0609a317158d4
maxim79135 <maxim79135@gmail.com>
2020-05-26 23:14:14 +0300

    preprocessing data and training the model

--- ChillWithPrograming/Anna Shilova/main.py
+++ ChillWithPrograming/Anna Shilova/main.py
@@ -2,5 +2,113 @@ import numpy as np
 import tensorflow as tf
 import pickle
 from tensorflow.keras import layers , activations , models , preprocessing
+import tensorflow.keras.utils as utils
+from gensim.models import Word2Vec
+import re
+import os
+import yaml
 
-print( tf.VERSION )
+dir_path = 'data'
+files_list = os.listdir(dir_path + os.sep)
+
+questions = list()
+answers = list()
+
+for filepath in files_list:
+    stream = open( dir_path + os.sep + filepath , 'rb')
+    docs = yaml.safe_load(stream)
+    conversations = docs['conversations']
+    for con in conversations:
+        if len( con ) > 2 :
+            questions.append(con[0])
+            replies = con[ 1 : ]
+            ans = ''
+            for rep in replies:
+                ans += ' ' + rep
+            answers.append( ans )
+        elif len( con )> 1:
+            questions.append(con[0])
+            answers.append(con[1])
+
+answers_with_tags = list()
+for i in range( len( answers ) ):
+    if type( answers[i] ) == str:
+        answers_with_tags.append( answers[i] )
+    else:
+        questions.pop( i )
+
+answers = list()
+for i in range( len( answers_with_tags ) ) :
+    answers.append( '<START> ' + answers_with_tags[i] + ' <END>' )
+
+tokenizer = preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts( questions + answers )
+VOCAB_SIZE = len( tokenizer.word_index )+1
+print( 'VOCAB SIZE : {}'.format( VOCAB_SIZE ))
+
+vocab = []
+for word in tokenizer.word_index:
+    vocab.append( word )
+
+def tokenize( sentences ):
+    tokens_list = []
+    vocabulary = []
+    for sentence in sentences:
+        sentence = sentence.lower()
+        sentence = re.sub( "[^a-zA-Z]", ' ', sentence )
+        tokens = sentence.split()
+        vocabulary += tokens
+        tokens_list.append( tokens )
+    return tokens_list , vocabulary
+
+p = tokenize( questions + answers )
+model = Word2Vec( p[ 0 ], min_count=1)
+print(model)
+
+#embedding_matrix = np.zeros( ( VOCAB_SIZE , 100 ) )
+#for i in range( len( tokenizer.word_index ) ):
+    #embedding_matrix[ i ] = model[ vocab[i] ]
+
+# encoder_input_data
+tokenized_questions = tokenizer.texts_to_sequences( questions )
+maxlen_questions = max( [ len(x) for x in tokenized_questions ] )
+padded_questions = preprocessing.sequence.pad_sequences( tokenized_questions , maxlen=maxlen_questions , padding='post' )
+encoder_input_data = np.array( padded_questions )
+print( encoder_input_data.shape , maxlen_questions )
+
+# decoder_input_data
+tokenized_answers = tokenizer.texts_to_sequences( answers )
+maxlen_answers = max( [ len(x) for x in tokenized_answers ] )
+padded_answers = preprocessing.sequence.pad_sequences( tokenized_answers , maxlen=maxlen_answers , padding='post' )
+decoder_input_data = np.array( padded_answers )
+print( decoder_input_data.shape , maxlen_answers )
+
+# decoder_output_data
+tokenized_answers = tokenizer.texts_to_sequences( answers )
+for i in range(len(tokenized_answers)) :
+    tokenized_answers[i] = tokenized_answers[i][1:]
+padded_answers = preprocessing.sequence.pad_sequences( tokenized_answers , maxlen=maxlen_answers , padding='post' )
+onehot_answers = utils.to_categorical( padded_answers , VOCAB_SIZE )
+decoder_output_data = np.array( onehot_answers )
+print( decoder_output_data.shape )
+
+
+encoder_inputs = tf.keras.layers.Input(shape=( None , ))
+encoder_embedding = tf.keras.layers.Embedding( VOCAB_SIZE, 200 , mask_zero=True ) (encoder_inputs)
+encoder_outputs , state_h , state_c = tf.keras.layers.LSTM( 200 , return_state=True )( encoder_embedding )
+encoder_states = [ state_h , state_c ]
+
+decoder_inputs = tf.keras.layers.Input(shape=( None ,  ))
+decoder_embedding = tf.keras.layers.Embedding( VOCAB_SIZE, 200 , mask_zero=True) (decoder_inputs)
+decoder_lstm = tf.keras.layers.LSTM( 200 , return_state=True , return_sequences=True )
+decoder_outputs , _ , _ = decoder_lstm ( decoder_embedding , initial_state=encoder_states )
+decoder_dense = tf.keras.layers.Dense( VOCAB_SIZE , activation=tf.keras.activations.softmax ) 
+output = decoder_dense ( decoder_outputs )
+
+model = tf.keras.models.Model([encoder_inputs, decoder_inputs], output )
+model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='categorical_crossentropy')
+
+model.summary()
+
+model.fit([encoder_input_data , decoder_input_data], decoder_output_data, batch_size=50, epochs=1 ) 
+model.save( 'model.h5' ) 
