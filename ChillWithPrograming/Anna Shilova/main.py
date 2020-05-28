b37feba9d09620b5fbc7b711ea6170e757ed2a72
maxim79135 <maxim79135@gmail.com>
2020-05-28 10:05:20 +0300

    unknow commit

--- ChillWithPrograming/Anna Shilova/main.py
+++ ChillWithPrograming/Anna Shilova/main.py
@@ -112,3 +112,55 @@ model.summary()
 
 model.fit([encoder_input_data , decoder_input_data], decoder_output_data, batch_size=50, epochs=1 ) 
 model.save( 'model.h5' ) 
+
+def make_inference_models():
+    
+    encoder_model = tf.keras.models.Model(encoder_inputs, encoder_states)
+    
+    decoder_state_input_h = tf.keras.layers.Input(shape=( 200 ,))
+    decoder_state_input_c = tf.keras.layers.Input(shape=( 200 ,))
+    
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    
+    decoder_outputs, state_h, state_c = decoder_lstm(
+        decoder_embedding , initial_state=decoder_states_inputs)
+    decoder_states = [state_h, state_c]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = tf.keras.models.Model(
+        [decoder_inputs] + decoder_states_inputs,
+        [decoder_outputs] + decoder_states)
+    
+    return encoder_model , decoder_model
+
+def str_to_tokens( sentence : str ):
+    words = sentence.lower().split()
+    tokens_list = list()
+    for word in words:
+        tokens_list.append( tokenizer.word_index[ word ] ) 
+    return preprocessing.sequence.pad_sequences( [tokens_list] , maxlen=maxlen_questions , padding='post')
+
+enc_model , dec_model = make_inference_models()
+
+for _ in range(10):
+    states_values = enc_model.predict( str_to_tokens( input( 'Enter question : ' ) ) )
+    empty_target_seq = np.zeros( ( 1 , 1 ) )
+    empty_target_seq[0, 0] = tokenizer.word_index['start']
+    stop_condition = False
+    decoded_translation = ''
+    while not stop_condition :
+        dec_outputs , h , c = dec_model.predict([ empty_target_seq ] + states_values )
+        sampled_word_index = np.argmax( dec_outputs[0, -1, :] )
+        sampled_word = None
+        for word , index in tokenizer.word_index.items() :
+            if sampled_word_index == index :
+                decoded_translation += ' {}'.format( word )
+                sampled_word = word
+        
+        if sampled_word == 'end' or len(decoded_translation.split()) > maxlen_answers:
+            stop_condition = True
+            
+        empty_target_seq = np.zeros( ( 1 , 1 ) )  
+        empty_target_seq[ 0 , 0 ] = sampled_word_index
+        states_values = [ h , c ] 
+
+    print( decoded_translation )
